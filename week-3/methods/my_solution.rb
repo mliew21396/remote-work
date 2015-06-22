7ba5058e3b03949443ce08aa7f05f6f4b980c450
Mathew Liew <mliew21396@gmail.com>
2015-06-22 15:22:50 -0500

    Add solution to 4-defining-method and previous exercise

--- week-3/methods/my_solution.rb
+++ week-3/methods/my_solution.rb
@@ -1,17 +1,26 @@
 # U1.W3: Methods
 
-# I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge by myself.
+# I spent 0.5 hours on this challenge.
 
 # Release 1: Try it out
 
 # 1. Full name greeting: Write a program that asks for a person's first name, then middle, then last. Finally, it should greet the person using their full name.
 
-# Your code here
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your middle name?"
+middle_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Your full name is " + first_name + " " + middle_name + " " + last_name + "!"
 
 # -------------------------------------------
 # 2. Bigger, better favorite number: Write a program that asks for a person's favorite number. Have your program add 1 to the number, and then suggest the result as a bigger and better number.
 
-# Your code here
+puts "What is your favorite number?"
+fav_num = gets.chomp
+new_num = fav_num.to_i + 1
+puts new_num.to_s + " is a bigger and better number."
 
 # -------------------------------------------
