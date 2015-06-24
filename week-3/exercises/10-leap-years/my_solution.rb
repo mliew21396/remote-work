732a0670d593012282e217080f0f076e049fe167
Mathew Liew <mliew21396@gmail.com>
2015-06-24 16:42:42 -0500

    Add exercises 7-11

--- week-3/exercises/10-leap-years/my_solution.rb
+++ week-3/exercises/10-leap-years/my_solution.rb
@@ -1,6 +1,16 @@
 # U1.W3: Leap Years
 
 # I worked on this challenge [by myself, with: ].
-
+def leap_year? (int)
+  if int%400==0
+    return true
+  elsif int%100==0
+    return false
+  elsif int%4 ==0
+    return true
+  else
+    return false
+  end
+end
 
 # Your Solution Below
--- week-3/exercises/11-simple-substrings/my_solution.rb
+++ week-3/exercises/11-simple-substrings/my_solution.rb
@@ -2,5 +2,11 @@
 
 # I worked on this challenge [by myself, with: ].
 
-
+def welcome (address)
+  if address.include?("CA")
+    return "Welcome to California"
+  else
+    return "You should move to California"
+  end
+end
 # Your Solution Below
