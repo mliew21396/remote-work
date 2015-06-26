289c2c206962e5fa017666e682c35c1b9f8d3c5e
Mathew Liew <mliew21396@gmail.com>
2015-06-26 18:41:48 -0500

    Add exercise solutions and the reflection for more methods

--- week-3/exercises/19-factorial/my_solution.rb
+++ week-3/exercises/19-factorial/my_solution.rb
@@ -5,5 +5,9 @@
 
 # Your Solution Below
 def factorial(number)
-  # Your code goes here
+  if number == 0
+    1
+  else
+    number * factorial(number-1)
+  end
 end
--- week-3/exercises/20-add-it-up/my_solution.rb
+++ week-3/exercises/20-add-it-up/my_solution.rb
@@ -9,30 +9,43 @@
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
 
-# Input:
-# Output:
+# Input: array of numbers
+# Output: the sum of all the numbers
 # Steps to solve the problem.
+#  Add first number to second number
+#  then keep adding the next number until there are no more
+#  Finally returns the sum
 
 
 # 1. total initial solution
-
-
+# def total (array)
+#   sum = 0
+#   array.each { |n| sum += n}
+# end
 
 # 3. total refactored solution
-
+def total (array)
+  # sum = 0
+  # array.each { |n| sum += n}
+  array.inject{|sum,n|sum+n}
+end
 
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input:array
+# Output:gramatically correct sentence
 # Steps to solve the problem.
-
+# Add each word to the sentence.
+# then make sure it has period at the end and first word is capitalized
 
 # 5. sentence_maker initial solution
-
+def sentence_maker(array)
+  initial_sent = array.inject { |sentence, word| sentence + " " + word.to_s}
+  return initial_sent.capitalize + "."
+end
 
 
 # 6. sentence_maker refactored solution
-
+#^^
 
